1d249337265d248d7235ecbf3870e64e42767e7f
GloopySoup <tayabz7935@gmail.com>
2026-02-08 13:13:14 +0000

    encryption

--- Main.py
+++ Main.py
@@ -1,10 +1,14 @@
 import mysql.connector
+from cryptography.fernet import Fernet
 db = mysql.connector.connect(
 host = "localhost",
 user="root",
 password="Red47red.",
 database = "pythonpasswordmanager")
 mycursor = db.cursor()
+key = b'P4IycxnVMm9JU_TMfC7ic20xIL1leeHQ3jq3nIgIceU='
+fernet = Fernet(key)
+encrypted_password = ""
 
 def check(value):
     mycursor.execute("SELECT EXISTS(SELECT * FROM password WHERE username = %s)",(value,))
@@ -14,7 +18,9 @@ def add_password(username, password):
     if  check(username):
         print("Account with that username already exists")
     else:
-        mycursor.execute("INSERT INTO password (username, password) VALUES(%s,%s)",(username, password))
+        encrypted_password = fernet.encrypt(password.encode())
+        print(encrypted_password)
+        mycursor.execute("INSERT INTO password (username, password) VALUES(%s,%s)",(username, encrypted_password))
         db.commit()
 
 
@@ -22,6 +28,8 @@ def get_password(username):
     if check(username):
         mycursor.execute("SELECT Password FROM pythonpasswordmanager.password WHERE username =%s",(username,))
         passw = mycursor.fetchone()[0]
+        passw = str(fernet.decrypt(passw))
+        passw = passw.strip("b'")
         print(f"The password for your account is: {passw}")
     else:
         print("Account doesn't exist")
